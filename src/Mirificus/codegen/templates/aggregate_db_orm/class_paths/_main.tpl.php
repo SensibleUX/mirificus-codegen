ac78ab2c3b172f2d021125978c7293f8d79cd95a
Jim Munro <jimmunro@quickenloans.com>
2013-10-04 08:51:50 -0400

    Cleanup codegen templats
    Making a concatenated string that gets echoed at the end. May need to turn
    these into functions.

--- src/Mirificus/codegen/templates/aggregate_db_orm/class_paths/_main.tpl.php
+++ src/Mirificus/codegen/templates/aggregate_db_orm/class_paths/_main.tpl.php
@@ -2,22 +2,24 @@
 	/**
 	 * Some sort of intelligent comment should probably go here...
 	 */
-	sprintf('<template OverwriteFlag="true" DocrootFlag="false" DirectorySuffix="" TargetDirectory="%s" TargetFileName="_class_paths.inc.php"/>', __MODEL_GEN__);
-	print("\n<?php\n");
+	$output = sprintf('<template OverwriteFlag="true" DocrootFlag="false" DirectorySuffix="" TargetDirectory="%s" TargetFileName="_class_paths.inc.php"/>', __MODEL_GEN__);
+	$output .= "\n<?php\n";
 	foreach ($objTableArray as $objTable) {
-		sprintf("// ClassPaths for the %s class \n", $objTable->ClassName);
+		$output .= sprintf("// ClassPaths for the %s class \n", $objTable->ClassName);
 		if (__MODEL__) {
-			sprintf('QApplicationBase::$ClassFile[\'%s\'] = __MODEL__ . \'%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
-			sprintf('QApplicationBase::$ClassFile[\'qqnode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
-			sprintf('QApplicationBase::$ClassFile[\'qqreversereferencenode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
+			$output .= sprintf('QApplicationBase::$ClassFile[\'%s\'] = __MODEL__ . \'%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
+			$output .= sprintf('QApplicationBase::$ClassFile[\'qqnode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
+			$output .= sprintf('QApplicationBase::$ClassFile[\'qqreversereferencenode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
 		}
 		if (__META_CONTROLS__) {
-			sprintf('QApplicationBase::$ClassFile[\'%smetacontrol\'] = __META_CONTROLS__ . \'/%sMetaControl.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
-			sprintf('QApplicationBase::$ClassFile[\'%sdatagrid\'] = __META_CONTROLS__ . \'/%sDataGrid.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
+			$output .= sprintf('QApplicationBase::$ClassFile[\'%smetacontrol\'] = __META_CONTROLS__ . \'/%sMetaControl.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
+			$output .= sprintf('QApplicationBase::$ClassFile[\'%sdatagrid\'] = __META_CONTROLS__ . \'/%sDataGrid.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
 		} 
 	}
+	
+	echo $output;
--- src/Mirificus/codegen/templates/aggregate_db_orm/class_paths/_main.tpl_1.php
+++ src/Mirificus/codegen/templates/aggregate_db_orm/class_paths/_main.tpl_1.php
@@ -1,24 +1,26 @@
 <?php
-	sprintf('<template OverwriteFlag="true" DocrootFlag="false" DirectorySuffix="" TargetDirectory="%s" TargetFileName="_class_paths.inc.php"/>', __MODEL_GEN__);
-	print("\n<?php\n");
+	$output = sprintf('<template OverwriteFlag="true" DocrootFlag="false" DirectorySuffix="" TargetDirectory="%s" TargetFileName="_class_paths.inc.php"/>', __MODEL_GEN__);
+	$output .= "\n<?php\n";
 
 	
 	foreach ($objTableArray as $objTable) {
-		sprintf('// ClassPaths for the %s class', $objTable->ClassName);
-		print("\n");
+		$output .= sprintf('// ClassPaths for the %s class', $objTable->ClassName);
+		$output .= "\n";
 		if (__MODEL__) {
-			sprintf('QApplicationBase::$ClassFile[\'%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
-			sprintf('QApplicationBase::$ClassFile[\'qqnode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
-			sprintf('QApplicationBase::$ClassFile[\'qqreversereferencenode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
+			$output .= sprintf('QApplicationBase::$ClassFile[\'%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
+			$output .= sprintf('QApplicationBase::$ClassFile[\'qqnode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
+			$output .= sprintf('QApplicationBase::$ClassFile[\'qqreversereferencenode%s\'] = __MODEL__ . \'/%s.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
 
 		}
 		if (__META_CONTROLS__) { 
-			sprintf('QApplicationBase::$ClassFile[\'%smetacontrol\'] = __META_CONTROLS__ . \'/%sMetaControl.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
-			sprintf('QApplicationBase::$ClassFile[\'%sdatagrid\'] = __META_CONTROLS__ . \'/%sDataGrid.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
-			print("\n");
+			$output .= sprintf('QApplicationBase::$ClassFile[\'%smetacontrol\'] = __META_CONTROLS__ . \'/%sMetaControl.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
+			$output .= sprintf('QApplicationBase::$ClassFile[\'%sdatagrid\'] = __META_CONTROLS__ . \'/%sDataGrid.class.php\';', strtolower($objTable->ClassName), $objTable->ClassName);
+			$output .= "\n";
 		}
 	}
+	
+	echo $output;
